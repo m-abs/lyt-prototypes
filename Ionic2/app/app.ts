b80cacd6946b8e892f63ac27b277e39d33d12b76
Daniel Freiling <dfg@nota.dk>
2016-02-23 19:51:54 +0100

    Added Ionic2 project

---
+++ Ionic2/app/app.ts
@@ -0,0 +1,36 @@
+import {App, Platform} from 'ionic-framework/ionic';
+
+// https://angular.io/docs/ts/latest/api/core/Type-interface.html
+import {Type} from 'angular2/core';
+
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic'
+
+@App({
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+})
+export class MyApp {
+  rootPage: Type = HelloIonicPage;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      console.log('=== PLATFORM READY ===');
+      console.log('Cordova', window.cordova);
+      console.log('Cordova', window.device);
+      // The platform is now ready. Note: if this callback fails to fire, follow
+      // the Troubleshooting guide for a number of possible solutions:
+      //
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      //
+      // First, let's hide the keyboard accessory bar (only works natively) since
+      // that's a better default:
+      //
+      // Keyboard.setAccessoryBarVisible(false);
+      //
+      // For example, we might change the StatusBar color. This one below is
+      // good for dark backgrounds and light text:
+      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+    });
+  }
+}
---
+++ Ionic2/app/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,100 @@
+'use strict';
+
+import {Page, Platform} from 'ionic-framework/ionic';
+
+@Page({
+  templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
+})
+export class HelloIonicPage {
+  platform: Platform;
+  media: Media;
+  isPlaying: boolean = false;
+  playPauseIcon: string = 'play';
+  currentPosition: number = 0;
+  duration: number = 0;
+
+  constructor(platform: Platform) {
+    this.platform = platform;
+    
+    setInterval(() => {
+      if (this.media) {
+        this.media.getCurrentPosition((position) => {
+          if (position > -1) {
+              this.currentPosition = Math.round(position * 100) / 100;
+              this.duration = Math.round(this.media.getDuration() * 100) / 100;
+              console.log(`Played to ${this.currentPosition} of ${this.duration} in media.`);
+          }
+        }, function() { console.log('fail'); });
+      }
+    }, 1000);
+  }
+
+  obtainNetworkConnection() {
+    this.platform.ready().then(() => {
+      console.log('obtainNetworkConnection :: platform.ready');
+        this.networkState = navigator.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        alert('Connection type: ' + states[this.networkState]);
+    });
+  }
+
+  playMedia() {
+    this.platform.ready().then(() => {
+      if (!this.media) {
+        // Plugin Docs: https://github.com/apache/cordova-plugin-media
+        // http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3
+        //this.media = new Media('http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3', function() { console.log('mediaSuccess') }, function() { console.log('mediaError') }, this.mediaStatusChange);
+        this.media = new Media('/android_asset/www/mp3/thunder.mp3',
+          function() { console.log('mediaSuccess'); },
+          function(err) { console.log('mediaError', err); },
+          this.mediaStatusChange);
+        console.log(this.media.setRate(2));
+        console.log('Created Media', this.media);
+      }
+      if (this.isPlaying) {
+        this.media.pause();
+        this.playPauseIcon = 'play';
+      } else {
+        this.media.setRate(2);
+        this.media.play();
+        this.playPauseIcon = 'pause';
+      }
+      this.isPlaying = !this.isPlaying;
+    });
+  }
+
+  stopMedia() {
+    if (this.media) {
+      this.media.stop();
+      this.media.release();
+      this.media = null;
+      this.isPlaying = false;
+    }
+  }
+
+  seekForward() {
+    if (this.media) {
+      console.log('seek');
+      this.media.getCurrentPosition((position) => {
+        if (position > -1) {
+          console.log((position) + " sec");
+          this.media.seekTo((position + 10) * 1000);
+        }
+      });
+    }
+  }
+
+  mediaStatusChange(status) {
+      console.log('Status change: '+ status);
+  }
+}
